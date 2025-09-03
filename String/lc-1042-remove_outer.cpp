6d07bedeb58699291a5a38eb8c75fb2ee95bf0ab
TalkeenAhmadNomani <talkeennomani@gmail.com>
2025-09-03 11:46:10 +0530

    optimized

--- String/lc-1042-remove_outer.cpp
+++ String/lc-1042-remove_outer.cpp
@@ -33,3 +33,36 @@ public:
         return ans;
     }
 };
+
+// optimizedclass Solution {
+public:{
+string removeOuterParentheses(string s)
+{
+    string result;
+    int balance = 0;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '(')
+        {
+
+            if (balance > 0)
+            {
+                result += s[i];
+            }
+            balance++;
+        }
+        else
+        {
+            balance--;
+            if (balance > 0)
+            {
+                result += s[i];
+            }
+        }
+    }
+
+    return result;
+}
+
+};
