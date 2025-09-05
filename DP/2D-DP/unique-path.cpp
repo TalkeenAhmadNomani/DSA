8fb01430e506bd44c5a190133f759c6a86b20896
TalkeenAhmadNomani <talkeennomani@gmail.com>
2025-09-05 20:52:53 +0530

    dp

--- DP/2D-DP/unique-path.cpp
+++ DP/2D-DP/unique-path.cpp
@@ -1,3 +1,30 @@
+class Solution
+{
+    int solve(int row, int col, vector<vector<int>> &dp)
+    {
+        if (row < 0 || col < 0)
+            return 0;
+        if (row == 0 && col == 0)
+            return 1;
+        if (dp[row][col] != -1)
+            return dp[row][col];
+
+        int up = solve(row - 1, col, dp);
+        int left = solve(row, col - 1, dp);
+
+        return dp[row][col] = up + left;
+    }
+
+public:
+    int uniquePaths(int m, int n)
+    {
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+
+        return solve(m - 1, n - 1, dp);
+    }
+};
+//optimized--
+
 public:
 int uniquePaths(int m, int n)
 {
