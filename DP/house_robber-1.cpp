9cbb3d8fb9e349e87b7c9383e0a9110391ce92d1
TalkeenAhmadNomani <talkeennomani@gmail.com>
2025-09-05 16:24:19 +0530

    dp

--- DP/house_robber-1.cpp
+++ DP/house_robber-1.cpp
@@ -1,3 +1,33 @@
+
+class Solution
+{
+    int solve(vector<int> &nums, int n, vector<int> &dp)
+    {
+        if (n < 0)
+            return 0;
+        if (n == 0)
+            return nums[0];
+        if (dp[n] != -1)
+            return dp[n];
+
+        int not_take = solve(nums, n - 1, dp);
+        int take = nums[n] + solve(nums, n - 2, dp);
+
+        return dp[n] = max(not_take, take);
+    }
+
+public:
+    int rob(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> dp(n + 1, -1);
+
+        return solve(nums, n - 1, dp);
+    }
+};
+
+//optimized
+
 class Solution
 {
 public:
