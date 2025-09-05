1ebeb5b6a1da1b07d0cdaa12e993e7355a81f264
TalkeenAhmadNomani <talkeennomani@gmail.com>
2025-09-05 11:44:39 +0530

    dp

---
+++ DP/stair.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+    int solve(int n)
+    {
+        if (n == 0 || n == 1)
+            return 1;
+
+        int one_step = solve(n - 1);
+        int two_step = solve(n - 2);
+
+        return one_step + two_step;
+    }
+
+public:
+    int climbStairs(int n)
+    {
+
+        return solve(n);
+    }
+};
+
+//optmized
+class Solution
+{
+
+public:
+    int climbStairs(int n)
+    {
+        vector<int> dp(n + 1, -1);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+};
