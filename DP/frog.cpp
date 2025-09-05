88b6e2f81cbdd6627d0e8c226089264b3a482db8
TalkeenAhmadNomani <talkeennomani@gmail.com>
2025-09-05 11:43:09 +0530

    dp

--- DP/frog.cpp
+++ DP/frog.cpp
@@ -1,10 +1,36 @@
+class Solution
+{
+    int solve(vector<int> &arr, int n, vector<int> &dp)
+    {
 
+        if (n == 0)
+            return 0;
+        if (dp[n] != -1)
+            return dp[n];
 
+        int one_step = solve(arr, n - 1, dp) + abs(arr[n] - arr[n - 1]);
 
+        int two_step = INT_MAX;
 
+        if (n - 2 >= 0)
+            two_step = solve(arr, n - 2, dp) + abs(arr[n] - arr[n - 2]);
 
+        return dp[n] = min(one_step, two_step);
+    }
 
-class Solution{
+public:
+    int minCost(vector<int> &height)
+    {
+        int n = height.size();
+        vector<int> dp(n + 1, -1);
+        return solve(height, n - 1, dp);
+    }
+};
+
+//optimized
+
+class Solution
+{
 public:
     int minCost(vector<int> &height)
     {
