0ab23e1b65bcaca19c7e498dc65c497f07574f50
TalkeenAhmadNomani <talkeennomani@gmail.com>
2025-09-06 10:42:09 +0530

    dp

--- DP/2D-DP/min-pathsum.cpp
+++ DP/2D-DP/min-pathsum.cpp
@@ -20,3 +20,32 @@ public:
         return solve(grid, m - 1, n - 1, dp);
     }
 };
+// optimized
+class Solution
+{
+public:
+    int minPathSum(vector<vector<int>> &grid)
+    {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[0][0] = grid[0][0];
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i == 0 && j == 0)
+                    continue;
+                int up = 1e9, left = 1e9;
+
+                if (i > 0)
+                    up = grid[i][j] + dp[i - 1][j];
+                if (j > 0)
+                    left = grid[i][j] + dp[i][j - 1];
+                dp[i][j] = min(up, left);
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
